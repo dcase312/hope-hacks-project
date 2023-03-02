140a5a82a57ad01c3147624b5b50d41e792a84f8
JosephVang099 <joseph-vang@outlook.com>
2023-03-02 09:24:53 -0500

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// let our server know the dbservice is there
+const dbService = require('./dbService');
+
+app.use(cors());
+app.use(express.json());
+
+//create
+app.post('/insert', (req,res) =>{})
+
+//read
+app.get('/getNames', (req,res)=>{
+const dbSQL = dbService.getDBService();
+ const results = dbSQL.getAllData();
+
+ console.log(results)
+})
+
+//update
+
+//delete
+
+app.listen(process.env.PORT, ()=> console.log('We Working'))
