898cc664698fa7f8dc72e9331109d233190fddbd
JosephVang099 <joseph-vang@outlook.com>
2023-03-06 16:15:22 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    loadTable([]);
-})
-function loadTable(data){
-    const table = document.querySelector('table tbody');
-    if(data.length === 0){
-        table.innerHTML = "<tr><td class ='no-data' colspan = '5'>No Data</td></tr>"
-    }
-}
